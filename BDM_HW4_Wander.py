277b9ad7f33d10e58751ec44185aadbb576ed13f
mwander000 <mwander000@citymail.cuny.edu>
2021-04-25 15:36:20 -0400

    testing

--- BDM_HW4_Wander.py
+++ BDM_HW4_Wander.py
@@ -8,52 +8,6 @@ import json
 import sys
 import os
 
-sc = pyspark.SparkContext()
-spark = SparkSession(sc)
-
-# user-added argument output folder path
-OUTPUT_PREFIX = sys.argv[1]
-
-rdd = sc.textFile('hdfs://data/share/bdm/core-places-nyc.csv')
-
-place_naics = {'452210': 'big_box_grocers', '452311': 'big_box_grocers', '445120': 'convenience_stores',
-               '722410': 'drinking_places', '722511': 'full_service_restaurants',
-               '722513': 'limited_service_restaurants', '446110': 'pharmacies_and_drug_stores',
-               '446191': 'pharmacies_and_drug_stores', '311811': 'snack_and_bakeries',
-               '722515': 'snack_and_bakeries', '445210': 'specialty_food_stores', '445220': 'specialty_food_stores',
-               '445230': 'specialty_food_stores', '445291': 'specialty_food_stores', '445292': 'specialty_food_stores',
-               '445299': 'supermarkets_except_convenience_stores'}
-
-# creating a dictionary of safegraph place ids for filtering on weekly patterns
-place_ids = dict(
-    rdd
-    .map(lambda e: next(csv.reader([e])))
-    .filter(lambda e: e[9] in place_naics.keys())
-    .map(lambda e: (e[1], e[9]))
-    .collect()
-)
-
-rdd = sc.textFile('hdfs://data/share/bdm/weekly-patterns-nyc-2019-2020/*')
-
-rdd \
-  .map(lambda e: next(csv.reader([e]))) \
-  .filter(lambda e: e[1] in place_ids.keys()) \
-  .map(lambda e: (place_naics.get(place_ids.get(e[1])), e[12], e[13], e[16])) \
-  .filter(lambda e: e[1] >= '2018-12-31T00:00:00-05:00' and e[2] <= '2021-01-04T00:00:00-05:00' ) \
-  .map(parseVisits) \
-  .flatMap(lambda e: e) \
-  .map(lambda e: ((e[0], e[1][0]), e[1][1])) \
-  .groupByKey() \
-  .map(prepare_rows) \
-  .toDF() \
-  .write \
-  .partitionBy('_1') \
-  .option('header', 'true') \
-  .csv(OUTPUT_PREFIX)
-
-for dirname in os.listdir(OUTPUT_PREFIX):
-    os.rename(OUTPUT_PREFIX + '/' + dirname, OUTPUT_PREFIX + '/' + dirname.replace('_1=', ''))
-
 def parseVisits(e):
     # e[1] == start_date
     # e[2] == end_date
@@ -90,3 +44,51 @@ def prepare_rows(e):
     date = str(e[0][1].replace(2020).date())
 
     return e[0][0], year, date, median, low, high
+
+sc = pyspark.SparkContext()
+spark = SparkSession(sc)
+
+# user-added argument output folder path
+OUTPUT_PREFIX = sys.argv[1]
+
+rdd = sc.textFile('hdfs://data/share/bdm/core-places-nyc.csv')
+
+place_naics = {'452210': 'big_box_grocers', '452311': 'big_box_grocers', '445120': 'convenience_stores',
+               '722410': 'drinking_places', '722511': 'full_service_restaurants',
+               '722513': 'limited_service_restaurants', '446110': 'pharmacies_and_drug_stores',
+               '446191': 'pharmacies_and_drug_stores', '311811': 'snack_and_bakeries',
+               '722515': 'snack_and_bakeries', '445210': 'specialty_food_stores', '445220': 'specialty_food_stores',
+               '445230': 'specialty_food_stores', '445291': 'specialty_food_stores', '445292': 'specialty_food_stores',
+               '445299': 'supermarkets_except_convenience_stores'}
+
+# creating a dictionary of safegraph place ids for filtering on weekly patterns
+place_ids = dict(
+    rdd
+    .map(lambda e: next(csv.reader([e])))
+    .filter(lambda e: e[9] in place_naics.keys())
+    .map(lambda e: (e[1], e[9]))
+    .collect()
+)
+
+rdd = sc.textFile('hdfs://data/share/bdm/weekly-patterns-nyc-2019-2020/*')
+
+rdd \
+  .map(lambda e: next(csv.reader([e]))) \
+  .filter(lambda e: e[1] in place_ids.keys()) \
+  .map(lambda e: (place_naics.get(place_ids.get(e[1])), e[12], e[13], e[16])) \
+  .filter(lambda e: e[1] >= '2018-12-31T00:00:00-05:00' and e[2] <= '2021-01-04T00:00:00-05:00') \
+  .map(parseVisits) \
+  .flatMap(lambda e: e) \
+  .map(lambda e: ((e[0], e[1][0]), e[1][1])) \
+  .groupByKey() \
+  .map(prepare_rows) \
+  .toDF() \
+  .write \
+  .partitionBy('_1') \
+  .option('header', 'true') \
+  .csv(OUTPUT_PREFIX)
+
+for dirname in os.listdir(OUTPUT_PREFIX):
+    os.rename(OUTPUT_PREFIX + '/' + dirname, OUTPUT_PREFIX + '/' + dirname.replace('_1=', ''))
+
+
