41ecdd6ce253c2fb81572023133c1c87c1256b92
mwander000 <mwander000@citymail.cuny.edu>
2021-04-28 00:30:06 -0400

    testing

--- BDM_HW4_Wander.py
+++ BDM_HW4_Wander.py
@@ -7,8 +7,19 @@ import json
 import sys
 import os
 
-if __name__ == '__main__':
-    def parse_visits(e):
+def extract_places(partId, records):
+    if partId==0:
+        next(records)
+    reader = csv.reader(records)
+    for row in reader:
+      if row[1] in place_ids.keys():
+        yield (place_naics.get(place_ids.get(row[1])), row[12], row[13], row[16])
+
+def fix_rows(partId, rows):
+  for e in rows:
+    yield ((e[0], e[1][0]), e[1][1])
+
+def parse_visits(e):
         # e[1] == start_date
         # e[2] == end_date
         start_date = datetime.datetime.strptime(e[1][:10], "%Y-%m-%d")
@@ -16,25 +27,37 @@ if __name__ == '__main__':
         visits_by_day = json.loads(e[3])
         return tuple(zip([e[0]] * len(dates), zip(dates, visits_by_day)))
 
+def parse_visits(partId, rows):
+  # e[1] == start_date
+  # e[2] == end_date
+  for e in rows:
+    start_date = datetime.datetime.strptime(e[1][:10], "%Y-%m-%d")
+    dates = [start_date + datetime.timedelta(days=n) for n in range(8)]
+    visits_by_day = json.loads(e[3])
+    visits_by_day = visits_by_day[0:4]
+    yield tuple(zip([e[0]]*len(dates), zip(dates, visits_by_day)))
 
-    def prepare_rows(e):
-        # e[1] == list of visits by day
-        truncate_neg = lambda x: x if x >= 0 else 0
+def prepare_rows(partId, rows):
+  # e[1] == list of visits by day 
+  for e in rows:
+    truncate_neg = lambda x: x if x >= 0 else 0
+    
+    # median visits by day value for date
+    median = int(np.median(tuple(e[1])))
+    std = int(np.std(tuple(e[1])))
+    low = int(truncate_neg(median - std))
+    high = int(median + std)
 
-        # median visits by day value for date
-        median = int(np.median(tuple(e[1])))
-        std = int(np.std(tuple(e[1])))
-        low = int(truncate_neg(median - std))
-        high = int(median + std)
+    # date and year
+    year = int(e[0][1].year)
 
-        # date and year
-        year = int(e[0][1].year)
+    # for projections onto 2020
+    date = str(e[0][1].replace(2020).date())
+  
+    yield (e[0][0], year, date, median, low, high)
 
-        # for projections onto 2020
-        date = str(e[0][1].replace(2020).date())
-
-        return e[0][0], year, date, median, low, high 
-
+if __name__ == '__main__':
+    
     # Initialize spark contexts
     sc = SparkContext()
     spark = SparkSession(sc)
@@ -63,14 +86,12 @@ if __name__ == '__main__':
     rdd = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/part-00000')
 
     rdd \
-      .map(lambda e: next(csv.reader([e.encode('utf-8')]))) \
-      .filter(lambda e: e[1] in place_ids.keys()) \
-      .map(lambda e: (place_naics.get(place_ids.get(e[1])), e[12], e[13], e[16])) \
-      .map(parse_visits) \
+      .mapPartitionsWithIndex(extract_places) \
+      .mapPartitionsWithIndex(parse_visits) \
       .flatMap(lambda e: e) \
-      .map(lambda e: ((e[0], e[1][0]), e[1][1])) \
+      .mapPartitionsWithIndex(fix_rows) \
       .groupByKey() \
-      .map(prepare_rows) \
+      .mapPartitionsWithIndex(prepare_rows) \
       .toDF(['category', 'year', 'date', 'median', 'low', 'high']) \
       .write \
       .partitionBy('category') \
