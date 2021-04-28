1dcb0f2072605393cc3fcb2dc0a3bbce9e601d04
mwander000 <mwander000@citymail.cuny.edu>
2021-04-27 22:19:50 -0400

    testing

--- BDM_HW4_Wander.py
+++ BDM_HW4_Wander.py
@@ -14,16 +14,6 @@ if __name__ == '__main__':
         start_date = datetime.datetime.strptime(e[1][:10], "%Y-%m-%d")
         dates = [start_date + datetime.timedelta(days=n) for n in range(8)]
         visits_by_day = json.loads(e[3])
-
-        if e[1] == '2018-12-31T00:00:00-05:00':
-            start_date = datetime.datetime(2019, 1, 1)
-            dates = [start_date + datetime.timedelta(days=n) for n in range(7)]
-            visits_by_day = visits_by_day[1:]
-        elif e[1] == '2020-12-28T00:00:00-05:00':
-            end_date = datetime.datetime(2020, 12, 31)
-            dates = [datetime.datetime(2020, 12, 28) + datetime.timedelta(days=n) for n in range(4)]
-            visits_by_day = visits_by_day[0:4]
-
         return tuple(zip([e[0]] * len(dates), zip(dates, visits_by_day)))
 
 
@@ -43,10 +33,11 @@ if __name__ == '__main__':
         # for projections onto 2020
         date = str(e[0][1].replace(2020).date())
 
-        return e[0][0], year, date, median, low, high
+        return e[0][0], year, date, median, low, high 
 
-
+    # Initialize spark contexts
     sc = SparkContext()
+    spark = SparkSession(sc)
 
     # user-added argument output folder path
     OUTPUT_PREFIX = sys.argv[1]
@@ -69,15 +60,12 @@ if __name__ == '__main__':
         .map(lambda e: (e[1], e[9]))
         .collect())
 
-    spark = SparkSession(sc)
-
     rdd = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*')
 
     rdd \
       .map(lambda e: next(csv.reader([e.encode('utf-8')]))) \
       .filter(lambda e: e[1] in place_ids.keys()) \
       .map(lambda e: (place_naics.get(place_ids.get(e[1])), e[12], e[13], e[16])) \
-      .filter(lambda e: e[1] >= '2018-12-31T00:00:00-05:00' and e[2] <= '2021-01-04T00:00:00-05:00') \
       .map(parse_visits) \
       .flatMap(lambda e: e) \
       .map(lambda e: ((e[0], e[1][0]), e[1][1])) \
